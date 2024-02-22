53ef538fcce7cc6079e5d8bb9bac331f7b1d06b6
Agustinmiquel <agumiquel@.com>
2024-02-22 15:26:15 -0300

    agregando .ENV directamente

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_REACT_APP_BACKEND_URL
-
 const clienteAxios = axios.create({
-    baseURL: baseURL,
+    baseURL: import.meta.env.VITE_REACT_APP_BACKEND_URL,
 })
 
 export default clienteAxios;
