0ada13323105527fb2c4b175f3d22b53c8fd8cfb
Agustinmiquel <agumiquel@.com>
2024-02-22 15:35:24 -0300

    editando el otro config

--- config/axios.js
+++ config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const clientesAxios = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: import.meta.env.VITE_REACT_APP_BACKEND_URL
 })
 
 export default clientesAxios;
