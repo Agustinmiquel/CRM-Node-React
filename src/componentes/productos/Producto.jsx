3a70d4f4ffbbac26398ede9b864ed62c32c76078
Agustinmiquel <agumiquel@.com>
2024-02-22 01:42:04 -0300

    producto-editarproducto

--- src/componentes/productos/Producto.jsx
+++ src/componentes/productos/Producto.jsx
@@ -44,7 +44,7 @@ export default function Producto({producto}) {
             <p className="nombre">{nombre}</p>
             <p className="precio">$ {precio}</p>
             { imagen ? (
-                <img src={`http://localhost:5000/${imagen}`} alt="imagen" />
+                <img src={`${import.meta.VITE_REACT_APP_BACKEND_URL}/${imagen}`} alt="imagen" />
             ) : null  }
         </div>
         <div className="acciones">
